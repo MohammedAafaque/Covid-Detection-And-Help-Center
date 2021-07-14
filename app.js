c375003092774425c48a93713e42176046a117bb
MohammedAafaque <aafaquemohammed96@gmail.com>
2021-07-14 16:19:04 +0530

    Added Covid Table To Map
    Implemented a table for the current covid cases across the globe.

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/source'));
+const fetch = require("node-fetch");
+
 
 
 app.get("/", function(req, res){
-    res.render("globalcases");
+    url = "https://corona.lmao.ninja/v2/countries";
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+    res.render("globalcases", {allData:data});
+    });
 })
 
 
