dcab8b6af1a77c7721cb1d2dbd4541b7457972a5
MohammedAafaque <aafaquemohammed96@gmail.com>
2021-07-15 18:56:41 +0530

    Added landing templates for all routes
    Implemented the landing page for all the routes

--- app.js
+++ app.js
@@ -6,14 +6,28 @@ app.use(express.static(__dirname + '/source'));
 const fetch = require("node-fetch");
 
 
-
 app.get("/", function(req, res){
+    res.render("index");
+})
+
+
+
+
+app.get("/dashboard", function(req, res){
     url = "https://corona.lmao.ninja/v2/countries";
     fetch(url).then((response) => {
         return response.json();
     }).then((data) => {
     res.render("globalcases", {allData:data});
     });
+})
+
+app.get("/resources", function(req,res){
+    res.render("covidsourcesmap");
+})
+
+app.get("/CoviCompanion", function(req, res){
+    res.render("chatbot");
 })
 
 
